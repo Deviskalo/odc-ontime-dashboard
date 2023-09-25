f8ab8c31e11542e1d869b59ecfab4db13bcfe6ef
Dev Iskalo <99690368+Deviskalo@users.noreply.github.com>
2023-09-25 09:29:04 +0000

    Adding all files

---
+++ src/components/Dashboard.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+const Dashboard = () => {
+  // Your chart data and options
+  const data = {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+    datasets: [
+      {
+        label: "Monthly Data",
+        data: [65, 59, 80, 81, 56, 55, 40, 75, 56, 70, 68, 90],
+        backgroundColor: "rgba(75,192,192,0.6)",
+        borderColor: "rgba(75,192,192,1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: "category", // Set the x-axis scale to "category"
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div className="flex mt-4 p-4 gap-4">
+      <div className="w-1/2 border-black border h-20 p-4 rounded">
+        <p>Hello world</p>
+        <p>Hello world</p>
+      </div>
+      <div className="w-1/2 border-black border h-20 p-4 rounded">
+        <p>Hello world</p>
+        <p>Hello world</p>
+      </div>
+      {/* Add your chart container here */}
+      <div className="w-1/2 border-black border h-20 p-4 rounded">
+        <Bar data={data} options={options} />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/Ontime Logo.png";
+import UserProfileImage from "../images/icons2.png";
+
+const Navbar = () => {
+  return (
+    <div className="bg-orange-600 p-4  flex justify-between items-center">
+      <div className="bg-white p-3 rounded-full">
+        <Link to="/">
+          <img src={Logo} alt="logo" className="w-15 h-6 " />
+        </Link>
+      </div>
+
+      <div className="text-white text-300 font-bold hover:orange space-x-6">
+        <Link to="/" className="hover:text-orange-300">
+          Dashboard
+        </Link>
+        <Link to="/attendance" className="hover:text-orange-300">
+          Attendance
+        </Link>
+        <Link to="/events" className="hover:text-orange-300">
+          Events
+        </Link>
+        <Link to="/activities" className="hover:text-orange-300">
+          Activities
+        </Link>
+      </div>
+
+      <div className="flex items-center space-x-4">
+        <div className="relative">
+          <div className="w-12 h-12 rounded-full border overflow-hidden">
+            <img
+              src={UserProfileImage}
+              alt="user-profile"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="absolute inset-0 bg-white opacity-50 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
