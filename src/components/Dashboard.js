dfc19c054b5e66dbe439831c698663479cf7dae0
Dev Iskalo <99690368+Deviskalo@users.noreply.github.com>
2023-09-29 13:20:22 +0000

    edited

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -58,17 +58,17 @@ const Dashboard = () => {
   return (
     <div className="mt-20">
       <div className="flex mt-4 p-4 gap-4">
-        <div className="w-1/2 flex border-black border justify-center align-center h-20 p-4 rounded-[20px]">
-          <p className="sm:font-bold tablet md:text-sm mt-0 absolute 2xl:text-bold pt-[1px]">
+        <div className="w-1/2 flex border-black border justify-center align-center h-20 p-4 rounded-[10px]">
+          <p className="font-bold md:text-sm mt-0 absolute 2xl:text-bold pt-[1px]">
             Total Logged In Users
           </p>
           <p className="my-4 text-center p-2">100</p>
         </div>
-        <div className="w-1/2 flex border-black border justify-center align-center h-20 p-4 rounded-[20px]">
+        <div className="w-1/2 flex border-black border justify-center align-center h-20 p-4 rounded-[10px]">
           <p className="font-bold mt-0 absolute  pt-[1px]">Total Events</p>
           <p className="my-4 text-center p-2">5</p>
         </div>
-        <div className="w-1/2 flex flex-col items-center border-black border h-20 p-4 rounded-[20px]">
+        <div className="w-1/2 flex flex-col items-center border-black border h-20 p-4 rounded-[10px]">
           <p className="font-bold mt-[-12px]">Gender</p>
           <div className="flex justify-between w-full">
             <div className="flex flex-col items-start">
@@ -82,8 +82,16 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      <div className="flex-1  m-4 border border-black border relative rounded-[20px] justify-center align-center">
+      <div className="flex  m-4 mt-1 border border-black border relative rounded-[10px] justify-center align-center">
         <canvas ref={canvasRef}></canvas>
+      </div>
+
+      <div className="flex gap-[19%] p-2 border-black m-4 h-40 border  relative justify-center rounded-[10px]">
+        <p>Name</p>
+        <p>Action</p>
+        <p>Users</p>
+        <p>Date Created</p>
+        <hr className="absolute bg-black w-full h-[2px] mt-[30px]" />
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ import UserProfileImage from "../images/icons2.png";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 bg-orange-600 p-4 flex justify-between items-center z-10">
-      <div className="bg-white p-3 rounded-full">
+    <div className="fixed top-0 left-0 right-0 bg-[#000] p-4 flex justify-between items-center z-10">
+      <div className="bg-white p-3 rounded-[10px]">
         <Link to="/">
           <img src={Logo} alt="logo" className="w-15 h-6" />
         </Link>
